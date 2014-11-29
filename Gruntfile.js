2adb174225b4df42bd9bdc457107bd0fb81b4f36
Sam Winter <sam.winter@gmail.com>
2014-11-28 19:32:28 -0500

    Working.

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        // files: {                         // Dictionary of files
+        //   'main.css': 'main.scss',       // 'destination': 'source'
+        //   'widgets.css': 'widgets.scss'
+        // }
+        files: [{
+          expand: true,
+          cwd: 'public/library/sass',
+          src: ['*.scss'],
+          dest: 'public/library/css',
+          ext: '.css'
+        }]
+      }
+    },
+    watch: {
+      files: ['public/library/sass/**/*.scss'],
+      tasks: ['sass']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+}
