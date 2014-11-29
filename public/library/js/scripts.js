740eeed9602bf64432b5274bdf82095994d8a213
Sam Winter <sam.winter@gmail.com>
2014-11-28 23:54:10 -0500

    More UI improvements.

--- public/library/js/scripts.js
+++ public/library/js/scripts.js
@@ -1,11 +1,24 @@
 talker = angular.module('Talker', []);
 
+var phrases = [
+'Hi', 
+'Hello', 
+'How are you?',
+'I am well thank you.',
+'Good morning', 
+'Good evening',
+'Good night',
+'Okey dokey',
+];
+
 talker.controller('TalkerController', ['$scope', function ($scope) {
 
 	$scope.text = '';
+	$scope.said = '';
 	$scope.supported = !!window.speechSynthesis;
+	if (!$scope.supported) return;
 	$scope.voices = [];
-	$scope.phrases = [];
+	$scope.phrases = phrases;
 	$scope.voice;
 
 	var speechSynthesis = window.speechSynthesis;
@@ -16,15 +29,28 @@ talker.controller('TalkerController', ['$scope', function ($scope) {
 		});
 	};
 
-	$scope.sayIt = function (phrase) {
+	$scope.sayIt = function (e, phrase) {
+		e.preventDefault();
 		if (!phrase) {
 			if ($scope.phrases.indexOf($scope.text) < 0) {
-				$scope.phrases.push($scope.text);	
+				$scope.phrases.push($scope.text);
 			}
 		}
-		var msg = new SpeechSynthesisUtterance(phrase || $scope.text);
+
+		var txt = phrase || $scope.text;
+		var msg = new SpeechSynthesisUtterance(txt);
 		msg.voice = $scope.voice;
 		speechSynthesis.speak(msg);
+		$scope.said = txt;
+	};
+
+	$scope.removePhrase = function (e, phrase) {
+		e.preventDefault();
+		e.stopPropagation();
+		var i = $scope.phrases.indexOf(phrase);
+		if (i > -1) {
+			$scope.phrases.splice(i, 1);
+		}
 	};
 
 	$scope.chooseVoice = function(voice) {
