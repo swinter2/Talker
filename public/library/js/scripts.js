01894c479a9b50d2651b4950aea9c4b9bbb70064
Sam Winter <sam.winter@gmail.com>
2014-11-28 21:21:04 -0500

    Working.

--- public/library/js/scripts.js
+++ public/library/js/scripts.js
@@ -1,14 +1,46 @@
+TalkerController = (function ($) {
 
-$(function () {
+	var supported = false, 
+		ss,
+		button,
+		textarea,
+		msg, 
+		voice;
 
-	var button = $("BUTTON");
-	var textarea = $("TEXTAREA");
+	function onDocReady() {
+		button = $("button");
+		textarea = $("textarea");
+		supported = window.speechSynthesis && SpeechSynthesisUtterance;
 
-	function sayIt() {
-		var msg = new SpeechSynthesisUtterance(textarea.val());
-		speechSynthesis.speak(msg);
+		if (!supported) {
+			console.error("No speech synthesis supported in this browser.");
+			button.add(textarea).hide();
+			textarea.after("No speech synthesis supported in this browser.");
+			return;
+		}
+		ss = window.speechSynthesis;
+
+		button.on('click', sayIt);
+		ss.onvoiceschanged = function () {
+			ss.getVoices().forEach(function (voice) {
+				console.log(voice.name);
+			});
+			voice = ss.getVoices().filter(function (voice) {
+				// return voice.name === 'Hysterical';
+				return voice.name === 'Google UK English Male';
+				// return voice.name === 'Google Deutsch';
+			})[0];
+			console.log(voice);
+		};
 	}
 
-	button.on('click', sayIt);
+	function sayIt() {
+		msg = new SpeechSynthesisUtterance(textarea.val());
+		msg.voice = voice;
+		ss.speak(msg);
+	}
 
-});
+	$(onDocReady);
+	return {};
+
+})(jQuery);
